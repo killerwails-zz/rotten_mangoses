cf891c71f024bcfb0863fcdb6d98e2fcf5dcce9e
Nikki <aimeeemm@hotmail.com>
2015-02-06 00:36:07 -0800

    woteva

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,7 +1,7 @@
 class Episode < ActiveRecord::Base
   
   has_attached_file :track
-  has_attached_file :avatar :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates :title,:description,:release_date, presence: true
 
